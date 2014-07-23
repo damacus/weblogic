124b36f31d34d26b912ca7667d90572addca0932
Michael Goetz <mpgoetz@gmail.com>
2014-07-23 08:16:42 -0700

    Reverting attributes file to previous version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,20 +1,22 @@
-default['weblogic']['oracle_base']                             = '/opt/oracle'
-default['weblogic']['user']                                    = 'weblogic'
-default['weblogic']['home']                                    = '/opt/oracle/middleware12c/wlserver_12.1'
-default['weblogic']['mw_home']                                 = '/opt/oracle/middleware12c'
-default['weblogic']['version']                                 = 1211
-default['weblogic']['orainventory']                            = '/opt/oracle/oraInventory'
-
-default['weblogic']['system_user']                             = 'wls'
-default['weblogic']['system_group']                            = 'oracle'
-default['weblogic']['instaler_source']                         = 'http://www.example.com/wls_121200.jar'
+default['weblogic']['user']                                    = 'oracle'
+default['weblogic']['group']                                   = 'oracle'
+default['weblogic']['beahome']                                 = '/home/oracle/oracle_mw'
+default['weblogic']['ocm_proxy']['host']                       = nil
+default['weblogic']['ocm_proxy']['port']                       = nil
+default['weblogic']['ocm_proxy']['user']                       = nil
+default['weblogic']['ocm_proxy']['password']                   = nil
+default['weblogic']['ocm_support']['email']                    = nil
+default['weblogic']['ocm_support']['password']                 = nil
+default['weblogic']['wls_install_dir']                         = '/home/oracle/oracle_mw/wlserver_12.1'
+default['weblogic']['ocp_install_dir']                         = '/home/oracle/oracle_mw/coherence_3.7'
+default['weblogic']['component_paths']                         = []
+default['weblogic']['install_node_manager_service']            = 'no'
+default['weblogic']['nodemgr_port']                            = 5556
+default['weblogic']['install_shortcut_in_all_users_folders']   = true
+default['weblogic']['local_jvms']                              = []
+default['weblogic']['bea_bundled_jvms']                        = []
+default['weblogic']['installer_download']                      = 'http://www.example.com/wls_121200.jar'
 default['weblogic']['installer_checksum']                      = 'e6efe85f3aec005ce037bd740f512e23c136635c63e20e02589ee0d0c50c065c'
-default['weblogic']['jdk_home']                                = '/usr/java/latest'
-default['weblogic']['log_dir']                                 = '/var/log/weblogic'
-
-default['weblogic']['domains_dir']                             = '/opt/oracle/wlsdomains/domains'
-default['weblogic']['apps_dir']                                = '/opt/oracle/wlsdomains/applications'
-
-default['weblogic']['jsse_enabled']                            = true
-
-default['weblogic']['nodemgr']['port']                         = 5556
+default['weblogic']['install_type']                            = 'WebLogic Server'
+default['oui']['inventory_loc']                                = '/home/oracle/orainventory'
+default['oui']['install_group']                                = 'oracle'
