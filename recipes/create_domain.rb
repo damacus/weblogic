1288991f1454c3041b51f89655c6e4ddcd011497
Bill Findley <wfindley@gmail.com>
2014-07-25 07:42:45 -0400

    initial commit of single simple domain create

--- recipes/create_domain.rb
+++ recipes/create_domain.rb
@@ -1,31 +1,31 @@
 
-domain_home_dirs = [
-  "#{node['weblogic']['domain_home'}",
-  "#{node['weblogic']['domain_home']}/#{node['weblogic']['domain_name']}",
-]
+# domain_home_dirs = [
+#   "#{node['weblogic']['domain_home'}" ,
+#   "#{node['weblogic']['domain_home']}/#{node['weblogic']['domain_name']}",
+# ]
 
-domain_home_dirs.each do |dirname|
-  directory dirname do
-    owner node['weblogic']['user'] 
-    group node['weblogic']['group'] 
-    mode 00755
-    action :create
-  end
+# domain_home_dirs.each do |dirname|
+#   directory dirname do
+#     owner node['weblogic']['user'] 
+#     group node['weblogic']['group'] 
+#     mode 00755
+#     action :create
+#   end
+# end
+
+directory node['weblogic']['domain_home'] do
+  owner node['weblogic']['user'] 
+  group node['weblogic']['group'] 
+  mode 00755
+  action :create
 end
 
-# directory node['weblogic']['domain_home'] do
-#   owner node['weblogic']['user'] 
-#   group node['weblogic']['group'] 
-#   mode 00755
-#   action :create
-# end
-
-# directory "#{node['weblogic']['domain_home']}/#{node['weblogic']['domain_name']}" do
-#   owner node['weblogic']['user'] 
-#   group node['weblogic']['group'] 
-#   mode 00755
-#   action :create
-# end
+directory "#{node['weblogic']['domain_home']}/#{node['weblogic']['domain_name']}" do
+  owner node['weblogic']['user'] 
+  group node['weblogic']['group'] 
+  mode 00755
+  action :create
+end
 
 template "#{node['weblogic']['domain_home']}/create_#{node[:weblogic][:domain_name]}_domain.py" do
   source "create_domain.py.erb"
@@ -35,7 +35,7 @@ template "#{node['weblogic']['domain_home']}/create_#{node[:weblogic][:domain_na
 end
 
 execute "wlst.sh create_#{node['weblogic']['domain_name']}_domain.py" do
-  command "#{node['weblogic']['wls_install_dir']}/common/bin/wlst.sh #{Chef::Config['file_cache_path']}/create_#{node['weblogic']['domain_name']}_domain.py"
+  command "#{node['weblogic']['wls_install_dir']}/common/bin/wlst.sh #{node['weblogic']['domain_home']}/create_#{node[:weblogic][:domain_name]}_domain.py"
   creates "#{node['weblogic']['domain_home']}/#{node['weblogic']['domain_name']}/bin"
   user node['weblogic']['user'] 
   group node['weblogic']['group'] 
