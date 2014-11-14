0ca863312404f311ff4f7c3e7395c76a3287924f
fooshards <fooshards@gmail.com>
2014-11-14 11:12:37 -0500

    Increased version to 0.2.0 -- Included support for 11g as well as 12c using attribute node['weblogic']['version']

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,15 @@
 Weblogic
 ========
 
-v0.1.3 (2014-05-16)
+v0.1.3 
 -------------------
-- Initial working recipe for installing Weblogic 12c
+- Initial working recipe for installing Weblogic 11g
 
 
-v0.1.1 (2014-04-19)
+v0.1.1 
 -------------------
 
 
-v1.0.0 (2014-04-19)
+v1.0.0 
 -------------------
 - Initial release
--- recipes/create_domain.rb
+++ recipes/create_domain.rb
@@ -1,41 +1,43 @@
 
-domain_home_dirs = [ 
-  "#{node['weblogic']['domain_home']}" ,
-  "#{node['weblogic']['domain_home']}/#{node['weblogic']['domain_name']}",
+domain_home_dirs = [
+  node['weblogic']['domain_home'],
+  "#{node['weblogic']['domain_home']}/#{node['weblogic']['domain_name']}"
 ]
 
 domain_home_dirs.each do |dirname|
   directory dirname do
-    owner node['weblogic']['user'] 
-    group node['weblogic']['group'] 
+    owner node['weblogic']['user']
+    group node['weblogic']['group']
     mode 00755
+    recursive true
     action :create
   end
 end
 
-template "#{node['weblogic']['domain_home']}/create_#{node[:weblogic][:domain_name]}_domain.py" do
-  source "create_domain.py.erb"
-  owner node['weblogic']['user'] 
-  group node['weblogic']['group'] 
-  mode "0755"
+template "#{node['weblogic']['domain_home']}/create_#{node['weblogic']['domain_name']}_domain.py" do
+  source 'create_domain.py.erb'
+  owner node['weblogic']['user']
+  group node['weblogic']['group']
+  mode '0755'
 end
 
 execute "wlst.sh create_#{node['weblogic']['domain_name']}_domain.py" do
-  command "#{node['weblogic']['wls_install_dir']}/common/bin/wlst.sh #{node['weblogic']['domain_home']}/create_#{node[:weblogic][:domain_name]}_domain.py"
+  command "#{node['weblogic']['wls_install_dir']}/common/bin/wlst.sh #{node['weblogic']['domain_home']}/create_#{node['weblogic']['domain_name']}_domain.py"
   creates "#{node['weblogic']['domain_home']}/#{node['weblogic']['domain_name']}/bin"
-  user node['weblogic']['user'] 
-  group node['weblogic']['group'] 
-  action :run
+  user node['weblogic']['user']
+  group node['weblogic']['group']
 end
 
-template "/etc/init.d/weblogic" do
-  source "weblogic_init.d.sh.erb"
-  owner "root"
-  group "root"
-  mode "0744"
-end
+if node['weblogic']['install_init_d']
+  template '/etc/init.d/weblogic' do
+    source 'weblogic_init.d.sh.erb'
+    owner 'root'
+    group 'root'
+    mode '0744'
+  end
 
-service "weblogic" do
-  supports :status => true, :restart => true, :reload => true
-  action [ :enable, :start ]
+  service 'weblogic' do
+    supports :status => true, :restart => false, :reload => false
+    action [:enable, :start]
+  end
 end
